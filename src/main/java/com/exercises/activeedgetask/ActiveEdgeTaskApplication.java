cc0dfff2160321440255c4757a83af053e59f65a
Realwale <olawaleagboola41@gmail.com>
2024-06-17 12:38:08 +0100

    initialize project

---
+++ src/main/java/com/exercises/activeedgetask/ActiveEdgeTaskApplication.java
@@ -0,0 +1,13 @@
+package com.exercises.activeedgetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveEdgeTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveEdgeTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercises/activeedgetask/ActiveEdgeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercises.activeedgetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveEdgeTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
